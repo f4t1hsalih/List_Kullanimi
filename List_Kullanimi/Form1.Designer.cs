087ccd3b480ac61451d20e9e6dc134dd2dddae59
f4t1hsalih <f4t1hsalih@gmail.com>
2023-09-04 20:33:25 +0300

    design complate

--- List_Kullanimi/Form1.Designer.cs
+++ List_Kullanimi/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 337);
+            button1.Name = "button1";
+            button1.Size = new Size(193, 35);
+            button1.TabIndex = 0;
+            button1.Text = "Listele 1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(225, 337);
+            button2.Name = "button2";
+            button2.Size = new Size(193, 35);
+            button2.TabIndex = 1;
+            button2.Text = "Listele 2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(437, 337);
+            button3.Name = "button3";
+            button3.Size = new Size(193, 35);
+            button3.TabIndex = 2;
+            button3.Text = "Listele 3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(193, 319);
+            listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 21;
+            listBox2.Location = new Point(225, 12);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(193, 319);
+            listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 21;
+            listBox3.Location = new Point(437, 12);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(193, 319);
+            listBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(211, 219, 193);
+            ClientSize = new Size(646, 386);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.FromArgb(34, 52, 77);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "List Kullanımı";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
     }
 }
